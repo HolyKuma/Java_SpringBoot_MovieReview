6d792549b138676153d7b3886f3a4669e8f806ac
Maximilian Eder <Maximilian.eder97@gmail.com>
2024-11-20 18:23:40 +0100

    git clone

---
+++ src/test/java/dev/eder/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eder.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
